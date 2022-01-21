f8915225293e06cf375e99d73b20f2151474e160
Developer0772 <alishernarzullayev7777@gmail.com>
2022-01-21 16:34:17 +0500

    optional search

--- src/components/navigation/file.js
+++ src/components/navigation/file.js
@@ -30,18 +30,20 @@ export default function MusicItem(props) {
   }
   const [click, setClick] = React.useState(false);
 
+  document.addEventListener(
+    "play",
+    function (e) {
+      var audios = document.getElementsByTagName("audio");
+      for (var i = 0, len = audios.length; i < len; i++) {
+        if (audios[i] !== e.target) {
+          audios[i].pause();
+        }
+      }
+    },
+    true
+  );
 
-  document.addEventListener('play', function(e){
-    var audios = document.getElementsByTagName('audio');
-    for(var i = 0, len = audios.length; i < len;i++){
-        if(audios[i] !== e.target){
-            audios[i].pause();
-          }
-    }
-}, true);
-
-
-  function DiscreteSliderLabel({click}) {
+  function DiscreteSliderLabel({ click }) {
     const [position, setPosition] = React.useState(0);
     function formatDuration(value) {
       const minute = Math.floor(value / 60);
@@ -50,65 +52,66 @@ export default function MusicItem(props) {
     }
     React.useEffect(() => {
       const timer = setInterval(() => {
-        setPosition(prev=>prev+1)
-      },1000);
+        setPosition((prev) => prev + 1);
+      }, 1000);
       return () => {
         clearTimeout(timer);
-      }
+      };
     }, []);
     return (
-      click &&
-      <Box
-        sx={{
-          width: "80%",
-          marginLeft: "10%",
-          position: "absolute",
-          bottom: "-20px",
-          fontSize: 2,
-        }}
-      >
-        <Slider
-          aria-label="time-indicator"
-          size="small"
-          value={position}
-          min={0}
-          step={1}
-          max={duration}
-          onChange={(_, value) => {
-            setPosition(value);
-            ref.current.currentTime = value;
-          }}
-          sx={{
-            height: 4,
-            "& .MuiSlider-thumb": {
-              width: 8,
-              height: 8,
-              transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
-              "&:before": {
-                boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
-              },
-              "&.Mui-active": {
-                width: 20,
-                height: 20,
-              },
-            },
-            "& .MuiSlider-rail": {
-              opacity: 0.28,
-            },
-          }}
-        />
+      click && (
         <Box
           sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            mt: -2,
+            width: "80%",
+            marginLeft: "10%",
+            position: "absolute",
+            bottom: "-20px",
+            fontSize: 2,
           }}
         >
-          <TinyText>{formatDuration(position)}</TinyText>
-          <TinyText>-{formatDuration(duration - position)}</TinyText>
+          <Slider
+            aria-label="time-indicator"
+            size="small"
+            value={position}
+            min={0}
+            step={1}
+            max={duration}
+            onChange={(_, value) => {
+              setPosition(value);
+              ref.current.currentTime = value;
+            }}
+            sx={{
+              height: 4,
+              "& .MuiSlider-thumb": {
+                width: 8,
+                height: 8,
+                transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+                "&:before": {
+                  boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
+                },
+                "&.Mui-active": {
+                  width: 20,
+                  height: 20,
+                },
+              },
+              "& .MuiSlider-rail": {
+                opacity: 0.28,
+              },
+            }}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              mt: -2,
+            }}
+          >
+            <TinyText>{formatDuration(position)}</TinyText>
+            <TinyText>-{formatDuration(duration - position)}</TinyText>
+          </Box>
         </Box>
-      </Box>
+      )
     );
   }
 
@@ -131,9 +134,9 @@ export default function MusicItem(props) {
               ref.current.pause();
             } else {
               setClick(true);
-              ref.current.play().catch(er=>{
+              ref.current.play().catch((er) => {
                 console.log(er);
-              })
+              });
               getDuration(function (length) {
                 setDuration(Math.floor(length));
               });
@@ -144,29 +147,33 @@ export default function MusicItem(props) {
         >
           {click ? <Pause fontSize="large" /> : <Play fontSize="large" />}
         </IconButton>
-        <ListItemText
-          style={{
-            textOverflow: "ellipsis",
-            overflow: "hidden",
-            whiteSpace: "nowrap",
-          }}
-          primary={props.name.replaceAll(`"`,"")
-            .replaceAll("&#039;", "'")
-            .replaceAll("&amp;", "&")
-          }
-          secondary={props.author.replaceAll(`"`,"")
-            .replaceAll("&#039;", "'")
-            .replaceAll("&amp;", "&")
-          }
-        />
-        <audio
-          style={{ display: "none" }}
-          src={props.url.replaceAll(`"`,"")}
-          controls
-          preload="none"
-          ref={ref}
-        ></audio>
-         <DiscreteSliderLabel click={click} />
+        {props && (
+          <ListItemText
+            style={{
+              textOverflow: "ellipsis",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+            }}
+            primary={props.name
+              .replaceAll(`"`, "")
+              .replaceAll("&#039;", "'")
+              .replaceAll("&amp;", "&")}
+            secondary={props.author
+              .replaceAll(`"`, "")
+              .replaceAll("&#039;", "'")
+              .replaceAll("&amp;", "&")}
+          />
+        )}
+        {props && (
+          <audio
+            style={{ display: "none" }}
+            src={props.url.replaceAll(`"`, "")}
+            controls
+            preload="none"
+            ref={ref}
+          ></audio>
+        )}
+        <DiscreteSliderLabel click={click} />
       </ListItem>
     </List>
   );
